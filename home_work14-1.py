0f3b396335ce388698a3868fc1d57c6acb18ae74
NadinBel <143278036+NadinBel@users.noreply.github.com>
2024-09-24 02:34:52 +0300

    Update home_work14-1.py

--- home_work14-1.py
+++ home_work14-1.py
@@ -6,7 +6,7 @@ cursor = connection.cursor()
 cursor.execute('''CREATE TABLE IF NOT EXISTS Users(id INTEGER PRIMARY KEY, username TEXT NOT NULL, 
 email TEXT NOT NULL, age INTEGER, balance INTEGER NOT NULL)''')
 
-'''for item in range(1, 11):
+for item in range(1, 11):
     cursor.execute("INSERT INTO users (username, email, age, balance) VALUES (?, ?, ?, ?)", (f'User{item}',
                                          f'example{item}@gmail.com', f'{item*10}', '1000',))
 
@@ -14,7 +14,7 @@ for i in range(1, 11, 2):
     cursor.execute("UPDATE Users SET balance = ? WHERE id = ?", (500, f'{i}'))
 
 for i in range(1, 11, 3):
-    cursor.execute("DELETE FROM Users WHERE id = ?", (f'{i}',))'''
+    cursor.execute("DELETE FROM Users WHERE id = ?", (f'{i}',))
 
 cursor.execute("SELECT username, email, age, balance FROM Users WHERE age != ?", (60,))
 data = cursor.fetchall()
